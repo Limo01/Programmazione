1d13996e5968a4a4df1c179c7b91562ab38a88c0
Limo01 <marcoandrea0145@gmail.com>
2020-03-19 19:23:59 +0100

    add esercizio1 in esercizi_a_tempo folder

---
+++ esercizi_a_tempo/esercizio1/esercizio1/es-tempo1.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+using namespace std;
+
+void stampa(int A[10][5])
+{
+	for (int r = 0; r < 10; r++)
+	{
+		for (int c = 0; c < 5; c++)
+		{
+			cout << A[r][c] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void stampa(bool A[10][5])
+{
+	for (int r = 0; r < 10; r++)
+	{
+		for (int c = 0; c < 5; c++)
+		{
+			cout << A[r][c] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void riempi(int A1[10][5], int A2[10][5])
+{
+	int i = 0;
+	int nInput;
+
+	int rigaA1 = 0;
+	int colonnaA1 = 0;
+
+	int rigaA2 = 0;
+	int colonnaA2 = 0;
+
+	while (i < 50)
+	{
+		cin >> nInput;
+
+		A1[rigaA1][colonnaA1] = nInput;
+		colonnaA1++;
+
+		A2[rigaA2][colonnaA2] = nInput;
+		rigaA2++;
+
+		if (colonnaA1 == 5)
+		{
+			rigaA1++;
+			colonnaA1 = 0;
+		}
+
+		if (rigaA2 == 10)
+		{
+			colonnaA2++;
+			rigaA2 = 0;
+		}
+		i++;
+	}
+
+	stampa(A1);
+	cout << endl;
+	stampa(A2);
+	cout << endl;
+}
+
+bool elementiComuni(int A1[10][5], int A2[10][5], int i, int j)
+{
+	bool elementiComuni = false;
+
+	for(int a=0; a < 5 && !elementiComuni; a++)//Ciclo 1
+	{
+		for(int b=0; b < 10 && !elementiComuni; b++)//Ciclo 2
+		{
+			elementiComuni = A1[i][a] == A2[b][j];
+		}
+	}
+
+	return elementiComuni;
+}
+
+
+void cross(int A1[10][5], int A2[10][5], bool B[10][5])
+{
+	for (int i = 0; i < 10; i++)//Ciclo 1
+	{
+		for (int j = 0; j < 5; j++)//Ciclo 2
+		{
+			B[i][j] = elementiComuni(A1, A2, i, j);
+		}
+	}
+	stampa(B);
+}
+
+int main()
+{
+	int A1[10][5], A2[10][5];
+	riempi(A1, A2);
+	bool B[10][5];
+	cross(A1, A2, B);
+}
+
+/*
+	INAVARIANTI CROSS:
+		INVARIANTE CICLO 1:	[R] = (0 <= i <= 10) & (i è l'indice della riga della matrice B)
+
+		INVARINATE CICLO 2: [R] = (0 <= i <= 10) & (0 <= j <= 5) & (i è l'indice della riga della matrice B) & (j è l'indice della colonna della matrice B)
+
+	INVARIANTI ELEMENTICOMUNI:
+		INVARIANTE CICLO 1:	[R] = (0 <= a <= 5) & (a è l'indice della colonna di B[i][]) & (elementiComuni == false => Non sono stati trovati elementi comuni) & (elementiComuni == true => Sono stati trovati elementi comuni)
+
+		INVARIANTE CICLO 2:	[R] = (0 <= a <= 5) & (0 <= b <= 10) & (a è l'indice della colonna di B[i][]) & (b è l'indice della riga di B[][j]) & (elementiComuni == false => Non sono stati trovati elementi comuni) & (elementiComuni == true => Sono stati trovati elementi comuni)
+*/
